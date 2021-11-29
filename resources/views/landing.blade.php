68a23a23cd11622c52d6bee35c7f401a5ea38266
rez <79359426+zoomy-git@users.noreply.github.com>
2021-11-29 19:35:13 +0700

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<<<<<<< Updated upstream
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href = "./resources/css/bootstrap-css" style="stylesheet">
+=======
+    @include('partials.import')
+    <style>
+        body{
+            
+            margin: auto;
+            padding-bottom: 10rem;
+            display: flex;
+            height: 100vh;
+            align-items: center;
+            justify-content: center;
+            
+            background-color: rgb(248, 245, 241, 0.5);
+        }
+
+        .light{
+            background-color: rgb(248, 245, 241);
+            
+            color: rgb(87, 68, 62);
+
+        }
+
+        .dark{
+            background-color: rgb(233, 137, 106);
+            color: rgb(248, 245, 241);
+        }
+
+    </style>
+>>>>>>> Stashed changes
     <title>Landing Page</title>
 </head>
 <body>
