2c3a20fe971b09a0142859afeb4088aec35d797f
rez <79359426+zoomy-git@users.noreply.github.com>
2021-11-30 06:29:39 +0700

    Update tentang.blade.php

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts/main')
 
-
 @section('header')
     @include('partials.close-button')
 @endsection
 
 @section('content')
-    <h1>Tentang</h1>
+
+    <div class="container d-flex flex-column">
+        <div class="container m-2 mt-4 text-center">
+            <h1><strong>Tentang Kami</strong></h1>
+        </div>
+        <div class="mx-5 my-4">
+            <h3>
+                <strong>your_average_company</strong>
+                , sebuah tim kecil yang Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, ut magni repudiandae consequatur quidem, libero praesentium dolores sunt ratione doloremque quaerat? Modi voluptas nihil quae, nemo repudiandae.
+            </h3>
+            <h3>
+                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos illo dolor odit. Exercitationem facilis laborum ratione sit fugit neque culpa vel. Maxime laudantium molestias eveniet, aut sunt ea aliquid quibusdam reiciendis provident dolores rerum odit quas ducimus qui architecto eligendi unde! Dolores quas pariatur tempora!
+            </h3>
+        </div>
+        
+    </div>
 @endsection
